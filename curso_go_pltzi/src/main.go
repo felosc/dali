1c5feb721bd6d15b0889a1b207faf39678484a65
felosc <49010129+felosc@users.noreply.github.com>
2023-02-15 11:06:27 -0500

    probando http go-zinsearch
    se prueba la subida de informacion por http con codugo de la doc de zincsearch
    #funciona

--- curso_go_pltzi/src/main.go
+++ curso_go_pltzi/src/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
 
+func main() {
+	data := `{
+        "Athlete": "DEMTSCHENKO, Albert",
+        "City": "Turin",
+        "Country": "RUS",
+        "Discipline": "Luge",
+        "Event": "Singles",
+        "Gender": "Men",
+        "Medal": "Silver",
+        "Season": "winter",
+        "Sport": "Luge",
+        "Year": 2006
+    }`
+	req, err := http.NewRequest("POST", "http://localhost:4080/api/games3/_doc", strings.NewReader(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth("admin", "Complex123")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	log.Println(resp.StatusCode)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
 }
